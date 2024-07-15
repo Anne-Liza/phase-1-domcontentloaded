6729ae41086123ff70fe8d6ce5158208e955905e
Anne <annewangui210@gmail.com>
2024-07-15 16:08:36 +0300

    phase-1-domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    const resultMessage = "This is really cool!";
+    document.getElementById("text").innerHTML= resultMessage;
+    
+});
